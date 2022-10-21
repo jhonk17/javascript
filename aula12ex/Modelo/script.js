658a6f3ebda277010a76d86e56c1070da28df53d
jhonk17 <jhon.santiagok17@gmail.com>
2022-10-20 21:43:41 -0300

    Estudo JS

---
+++ aula12ex/Modelo/script.js
@@ -0,0 +1,21 @@
+function carregar(){
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 13
+    msg.innerHTML = `Agora são ${hora} horas`
+    if(hora >= 0 && hora < 12){
+        //Bom dia
+        img.src = 'Imagens Projetos/Imgmanha1.png'
+        document.body.style.backgroundColor = '#e3c29d'
+    } else if (hora >= 12 && hora < 18){
+        //Boa tarde
+        img.src = 'Imagens Projetos/img2tarde2.png'
+        document.body.style.backgroundColor = '#f4a869'
+    } else {
+        //Boa noite
+        img.src = 'Imagens Projetos/img1.png'
+        document.body.style.backgroundColor = '#2f2f2f'
+    }
+}
