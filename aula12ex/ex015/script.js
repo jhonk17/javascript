99298c399601734ded6dd2f7d819f5313a384637
jhonk17 <jhon.santiagok17@gmail.com>
2022-10-27 22:18:36 -0300

    Estudo dia 27

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -3,9 +3,43 @@ function verificar(){
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
     var res = document.getElementById('res')
-    if (fano.value.length == 0 || fano.value > ano){
+    if (fano.value.length == 0 || Number(fano.value) > ano){
         window.alert('Verifique os dados e tente novamente"')
-    }else{
-        window.alert('tudo ok')
+    }else{ 
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >=0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'image/criança homem.png')
+            }else if (idade < 21) {
+                //jovem
+            }else if (idade < 50){
+                //Adulto
+            }else{
+                //Idoso
+            }
+                
+        } else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >=0 && idade < 10){
+                //Criança
+            }else if (idade < 21) {
+                //jovem
+            }else if (idade < 50){
+                //Adulto
+            }else{
+                //Idoso
+            }
+                
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Dectectamos ${genero} com ${idade} Anos`
+        res.appendChild(img).style.height = '250px'
+        res.appendChild(img).style.width = '250px'
     }
 }
