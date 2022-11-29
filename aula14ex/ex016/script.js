855665199041259fe2573759f4d64f3f4078d1ef
jhonk17 <jhon.santiagok17@gmail.com>
2022-11-28 21:39:56 -0300

    Estudo 28/07
    Terminando exercício JS curso em vídeo

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -2,11 +2,32 @@ function contar(){
     let ini = document.getElementById('txt1')
     let fim = document.getElementById('txt2')
     let passo = document.getElementById('txt3')
-    
+    let res = document.getElementById('res')
 
-    if (ini.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght ==0){
-        alert('Erro')
-    }else{
-        alert('Okk')
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+        alert('[Erro] Faltam dados!')
+        
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }   
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
 }
